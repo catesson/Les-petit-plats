362df56b9e222287db8f5b6de7a74d85511abbad
steven catesson <steven69700@gmail.com>
2022-12-07 12:29:34 +0100

    ajout de la fonctionnalité de mise à jours des tag en fonction des recettes restante

--- js/recherche/search.js
+++ js/recherche/search.js
@@ -11,17 +11,20 @@ export const search = (recettes) => {
   if (searchText.length >= 3) {
       //supprime toutes les recettes afficher
       deleteRecettesDom();
+      const findRecherche = []
     // pour toutes les recherche de la base de donnée
     recettes.forEach((recette) => {
       const name = recette.name.toLowerCase();
       // ajoute la recette s'il y a correspondance dans le titre
       if (name.includes(searchText)) {
         createRecettesCards(recette);
+        findRecherche.push(recette);
       } else {
         const description = recette.description.toLowerCase();
         // ajoute la recette s'il y a correspondance dans la description
         if (description.includes(searchText)) {
           createRecettesCards(recette);
+          findRecherche.push(recette);
         } else {
           //création du tableau d'ingrédient
           const ingrédient = [];
@@ -37,12 +40,19 @@ export const search = (recettes) => {
           // ajoute la recette s'il y a correspondance dans les ingrédient
           if (allIngrédients.includes(searchText)) {
             createRecettesCards(recette);
+            findRecherche.push(recette);
           }
         }
       }
     });
+    if (findRecherche.length === 0){
+      const message = document.querySelector("#no-recette");
+      message.innerHTML = "<p>Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.</p>"
+    }
+    return findRecherche
   }
   else {
     displayRecette(recettes);
+    return recettes;
   }
 };
